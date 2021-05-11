5f5262f0f4594d3011a790c4f2c43c5890a43a02
simondifalco13 <61333124+simondifalco13@users.noreply.github.com>
2021-05-11 19:44:46 +0100

    seq diag + modif structure asp

---
+++ commun/Réunion 8/ASP_PROJECT/ASP_PROJECT/Models/POCO/Restorer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_PROJECT.Models.Project
+{
+    public class Restorer : Account
+    {
+
+        public List<Restaurant> restaurantList;
+
+        public Restorer() : base()
+        {
+
+        }
+    }
+}
---
+++ commun/Réunion 8/ASP_PROJECT/ASP_PROJECT/ViewModels/Menu/ListDishViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ASP_PROJECT.Models.Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_PROJECT.ViewModels.Simon
+{
+    public class ListDishViewModel
+    {
+        public List<Dish> ListDishesDb;
+
+        public ListDishViewModel(List<Dish> l)
+        {
+            ListDishesDb = l;
+        }
+
+    }
+}
