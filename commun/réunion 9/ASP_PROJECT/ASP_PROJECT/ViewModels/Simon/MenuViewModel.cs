f08ce381ff1a919c1746699aee535c880a31b1bd
Usagi08 <62385585+Usagi08@users.noreply.github.com>
2021-05-11 17:38:44 +0200

    Création réunion 9

---
+++ commun/réunion 9/ASP_PROJECT/ASP_PROJECT/ViewModels/Simon/MenuViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ASP_PROJECT.Models.Project;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_PROJECT.ViewModels.Simon
+{
+    public class MenuViewModel
+    {
+        public List<Dish> Dlist;
+        public Menu Menu { get; set; }
+        public Dish SelectedDish { get; set; }
+        public MenuViewModel()
+        {
+            Menu = new Menu();
+            Dlist = new List<Dish>();
+            MenuDishList = new List<Dish>();
+        }
+
+        public MenuViewModel(List<Dish> l) :this()
+        {
+            Dlist = l;
+            //foreach (var d in l)
+            //{
+            //    DishesList.Add(new SelectListItem() { Value = d.Name, Text = d.Name });
+            //}
+        }
+
+       
+        public List<SelectListItem> TypeService { get; set; } = new List<SelectListItem>
+        {
+            new SelectListItem(){Value="Lunch",Text="Diner"},
+            new SelectListItem(){Value="Evening",Text="Soir"}
+        };
+
+        public List<SelectListItem> DishesList { get; set; } = new List<SelectListItem>();
+
+        public List<Dish> MenuDishList;
+    }
+}
