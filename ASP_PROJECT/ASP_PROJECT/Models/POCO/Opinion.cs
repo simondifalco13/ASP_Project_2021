928ba06f321c01a1ec8c4575a6df2b2dea1db354
simondifalco13 <61333124+simondifalco13@users.noreply.github.com>
2021-05-16 11:05:27 +0100

    modifs

---
+++ ASP_PROJECT/ASP_PROJECT/Models/POCO/Opinion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_PROJECT.Models.POCO
+{ 
+    public class Opinion
+    {
+        [Display(Name = "Commentaire")]
+        [StringLength(500)]
+        public string OpinionDescription { get; set; }
+
+        [Display(Name = "Note")]
+        [Required(ErrorMessage = "Une note est obligatoire")]
+        public int Rating { get; set; }
+
+        public Opinion()
+        {
+
+        }
+        public void ModerateOpinion()
+        {
+
+        }
+    }
+}
