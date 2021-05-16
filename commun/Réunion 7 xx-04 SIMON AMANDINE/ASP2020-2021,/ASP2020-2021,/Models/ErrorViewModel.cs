ad1fcd7ef5f5bdc5c5b96334992de56706a64367
Usagi08 <62385585+Usagi08@users.noreply.github.com>
2021-05-16 11:26:15 +0200

    suppression main

--- commun/Réunion 7 xx-04 SIMON AMANDINE/ASP2020-2021,/ASP2020-2021,/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ASP2020_2021_.Models {
-    public class ErrorViewModel {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
