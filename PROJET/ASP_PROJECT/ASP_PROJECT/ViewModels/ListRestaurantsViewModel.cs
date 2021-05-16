826d25712f7f1b45e7fe86ed9b1c0f6074ca2789
Usagi08 <62385585+Usagi08@users.noreply.github.com>
2021-05-16 20:01:45 +0200

    dd

--- PROJET/ASP_PROJECT/ASP_PROJECT/ViewModels/ListRestaurantsViewModel.cs
+++ PROJET/ASP_PROJECT/ASP_PROJECT/ViewModels/ListRestaurantsViewModel.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace ASP_PROJECT.ViewModels {
     public class ListRestaurantsViewModel {
-     
+
+        public Restaurant Resto { get; set; }
+
+        public ListRestaurantsViewModel(){
+            Resto = new Restaurant();    
+        }
     }
 }
